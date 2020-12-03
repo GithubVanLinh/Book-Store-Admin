6450d8521e7ff079153a1c31ac6ace7a645956f6
kptankhoa <kptankhoa@gmail.com>
2020-12-03 12:18:40 +0700

    add createbook form

--- controllers/book.controller.js
+++ controllers/book.controller.js
@@ -4,7 +4,7 @@ module.exports = {
     createANewBook:async (req, res, next)=>{
         const aNewBookInfo = req.body;
         console.log("a New Book Info: ", aNewBookInfo);
-        const newBookRes =await Book.createANewBook(aNewBookInfo);
+        const newBookRes = await Book.createANewBook(aNewBookInfo);
         console.log("a Book Res: ", newBookRes);
         if(newBookRes == -1){
             res.send("Create Failed");
